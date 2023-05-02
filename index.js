2eefbe0eeb2472f9ea697cc63e2c2ace5a35e9c9
Marni Luka <marnixluka@gmail.com>
2023-05-01 20:06:23 -0400

    edited the logo template and added text to the shapes because I forgot

--- index.js
+++ index.js
@@ -47,12 +47,21 @@ const shapeSquare = logoShapes.shapeSquare;
 
 const logoTemplate = (responses) => {
     if (responses.shape === 'Circle') {
-        return `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200"> ${shapeCircle(responses.shapeColor)}`
+        return `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200"> 
+        <circle cx="115" cy="165" r="110" fill="${responses.shapeColor}" />
+        <text x="115" y="200" font-size="100" text-anchor="middle" fill="${responses.textColor}">${responses.text}</text>
+        </svg>`
     }
     if (responses.shape === 'Triangle') {
-        return `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200"> ${shapeTriangle(responses.shapeColor)}`
+        return `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
+        <polygon points = "150, 18 244, 182 56, 182" fill="${responses.shapeColor}" /> 
+        <text x="145" y="150" font-size="80" text-anchor="middle" fill="${responses.textColor}">${responses.text}</text>
+        </svg>`
     }
     if (responses.shape === 'Square') {
-        return `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200"> ${shapeSquare(responses.shapeColor)}`
+        return `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
+        <rect width="200" height="200" fill="${responses.shapeColor}" />
+        <text x="145" y="150" font-size="120" text-anchor="middle" fill="${responses.textColor}">${responses.text}</text>
+        </svg>`
     }
 }
